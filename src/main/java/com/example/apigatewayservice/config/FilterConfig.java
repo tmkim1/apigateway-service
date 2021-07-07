ab5745e357ac8a136c554036814e23a43a5ea8c7
kimtaemin <xoalstjtm@gmail.com>
2021-07-07 20:20:48 +0900

    Spring Cloud Gateway - Filter 적용

---
+++ src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.example.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    // r -> r.path("/first-service/**" 해당 관련 요청이 들어오면
+    // .uri("http://localhost:8081") 설정한 uri 호출
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                            .filters(f -> f.addRequestHeader("first-request","first-request-header")
+                                           .addResponseHeader("first-response","first-response-header"))
+                            .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request","second-request-header")
+                                .addResponseHeader("second-response","second-response-header"))
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+}
