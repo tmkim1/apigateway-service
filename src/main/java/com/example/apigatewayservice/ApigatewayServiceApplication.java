d4e5009ef2f9389ab08e47d51cb38ee2e23378c6
kimtaemin <xoalstjtm@gmail.com>
2021-07-21 21:42:34 +0900

    Spring Cloud Config 적용

--- src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
@@ -10,4 +13,8 @@ public class ApigatewayServiceApplication {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();      //client 요청했던 트레이 정보가 메모리에 담겨 해당 값을 사용할수 있게 됨.
+    }
 }
